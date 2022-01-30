b4e6eb6f12ea6299a9e0abd88e49149a84055f1e
Vsevolod <mr.freia@mail.ru>
2022-01-30 15:56:58 +0300

    update decorator and generator tine test

--- UDEMY tasks/decorator_demo_test.py
+++ UDEMY tasks/decorator_demo_test.py
@@ -15,7 +15,12 @@ def speed_time(function):
 def sum_lst():
     return sum([n for n in range(100000000)])
 
+@speed_time
+def sum_generator():
+    return sum((n for n in range(100000000)))
+
 print(sum_lst)
+print(sum_generator)
 
 
 
